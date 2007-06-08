f3a2d72116bc86c7b1b29fa29c79b721e0a94f17
jjrojoc <jjrojoc>
2007-06-08 00:41:59 +0000

    Added delete linea from ticketstore

--- trunk/pytpv/src/pytpv.py
+++ trunk/pytpv/src/pytpv.py
@@ -95,7 +95,7 @@ class Acredita:
             
         
         
-        self.ticketstore = gtk.ListStore(gobject.TYPE_STRING, #ID_TICKET
+        self.ticketstore = gtk.ListStore(gobject.TYPE_INT, #ID_TICKET
                                          gobject.TYPE_STRING, # CATIDAD
                                          gobject.TYPE_STRING,  #DESCRIPCION
                                          gobject.TYPE_STRING)    # Importe    
@@ -111,12 +111,12 @@ class Acredita:
         for j in range(len(columnsticketview)):
             render = gtk.CellRendererText()
             render.set_property('editable', True)
-            render.connect('edited', self.editedcells, j)
+            render.connect('edited', self.editedcells, j+1)
             columna = gtk.TreeViewColumn(columnsticketview[j], render, text=(j+1))
             columna.set_resizable(True)
             self.treeview3.append_column(columna)
-            columna.set_sort_column_id(j)
-            render.connect('edited', self.editedcells, j)  
+            columna.set_sort_column_id(j+1)
+            render.connect('edited', self.editedcells, j+1)  
         
         # retoque de la GUI que Glade no permite
         
@@ -124,11 +124,7 @@ class Acredita:
         # unidades por pagina para la impresion
         self.upp = 1
         
-        
-        
     def buscar(self, datos=None):
-        
-        
         self.listStore.clear()
         c = self.cursor
         #c.execute('select id, nombre, direccion, importe, hora from acreditaciones where nombre = %s', self.widgets.get_widget('entBusqueda').get_text())
@@ -141,7 +137,6 @@ class Acredita:
             
             self.listStore.append([id]+dato)    
         
-
         
     def cargaDatos(self, consulta):
         c = self.cursor
@@ -154,115 +149,7 @@ class Acredita:
             
             self.listStore.append([id]+dato)
 
-    def generaAcreditacion(self, canvas, iter, x, y):
-        colores = {
-            'Visitante': ((0.36, 0.36, 0.36), (0.89, 0.89, 0.89)),
-            'Ponente'  : ((0.91, 0.19, 0.19), (0.92, 0.69, 0.69)),
-            'Organizacion' : ((0, 0.48, 0.77), (0.45, 0.76, 0.93))
-            }
-        iconos = {
-            'Visitante'    : 'icono-visitante.png',
-            'Ponente'      : 'icono-ponente.png',
-            'Organizacion' : 'icono-organizacion.png'
-            }
-        nombre = self.listStore.get_value(iter, NOMBRE)
-        if nombre: nombre = nombre.encode('latin-1')
-        apellidos = self.listStore.get_value(iter, DIRECCION)
-        if apellidos: apellidos = apellidos.encode('latin-1')
-        ciudad = self.listStore.get_value(iter, IMPORTE)
-        if ciudad: ciudad = ciudad.encode('latin-1')
-        if len(ciudad) > 15:
-            ciudad = ciudad[:15] + '...'
-        grupo = self.listStore.get_value(iter, ORGANIZACION)
-        if grupo: grupo = grupo.encode('latin-1')
-        if len(grupo) > 20:
-            grupo = grupo[:20] + '...'
-        rol = self.listStore.get_value(iter, ROL)
-        if rol: rol = rol.encode('latin-1')
-
-        icono = Image.open(iconos[rol])
-        iconoAncho, iconoAlto = icono.size            
             
-        canvas.translate(x, y)
-        # dibujo los rectangulos de fondo
-        color = colores[rol][0]
-        canvas.setFillColorRGB(color[0], color[1], color[2])
-        canvas.rect(0, 0, ANCHO, ALTO, stroke=1, fill=1)
-        color = colores[rol][1]
-        canvas.setFillColorRGB(color[0], color[1], color[2])
-        canvas.rect(2*mm, 2*mm, ANCHO-4*mm, ALTO-6*mm-iconoAlto,
-                    stroke=0, fill=1)
-        
-        # ahora el icono
-        canvas.drawImage(icono, 2*mm, ALTO-2*mm-iconoAlto)
-            
-        # dibujo el texto comun
-        canvas.setFillColorRGB(1, 1, 1)
-        canvas.setFont('Helvetica', 16)
-        canvas.drawString(10 + iconoAncho, ALTO-5*mm, 'III')
-        canvas.drawString(10 + iconoAncho, ALTO-10*mm, 'jornadas')
-        canvas.drawString(10 + iconoAncho, ALTO-15*mm, 'andaluzas')
-        canvas.setFont('Helvetica', 12)
-        canvas.drawString(10 + iconoAncho, ALTO-20*mm, 'de Software Libre')
-        canvas.setFont('Helvetica', 10)
-        canvas.setFillColorRGB(0, 0, 0)
-        canvas.drawCentredString(ANCHO/2, ALTO-25*mm,
-                            'Granada. 14 y 15 de Noviembre')
-
-        canvas.setFont('Helvetica', 18)
-        canvas.drawCentredString(ANCHO/2, 35*mm, nombre)
-        canvas.drawCentredString(ANCHO/2, 28*mm, apellidos)
-        canvas.line(5*mm, 26*mm, ANCHO-5*mm, 26*mm)
-        canvas.drawCentredString(ANCHO/2, 20*mm, ciudad)
-        if grupo != '' and grupo != None:
-            canvas.setFont('Helvetica', 14)
-            canvas.drawCentredString(ANCHO/2, 15*mm, grupo)
-            
-        canvas.setFont('Helvetica', 12)
-        canvas.drawString(3*mm, 5*mm, rol)
-
-        canvas.translate(-x, -y)
-
-    def creaPDF(self):
-        ancho, alto = A4
-        c = canvas.Canvas(NOMBRE_FICHERO, pagesize=A4)
-        seleccion = []
-        self.listView.get_selection().selected_foreach(
-            lambda model, path, iter, sel = seleccion: sel.append(iter))
-        i = 0
-        x = MARGEN
-        y = alto - MARGEN - ALTO
-        for iter in seleccion:
-            self.generaAcreditacion(c, iter, x, y)
-            i += 1
-            if i >= self.upp:
-                i = 0
-                x = MARGEN
-                y = alto - MARGEN - ALTO
-                c.showPage()
-                continue
-            
-            x += (ANCHO + 1*mm)
-            if x + ANCHO > (ancho - MARGEN):
-                x = MARGEN
-                y -= (ALTO + 1*mm)
-
-            if y < MARGEN:
-                x = MARGEN
-                y = alto - MARGEN - ALTO
-                i = 0
-                c.showPage()
-                
-        c.save()
-        
-    def vistaPrevia(self, boton, datos=None):
-        self.creaPDF()
-        os.system('xpdf %s' % NOMBRE_FICHERO)
-
-    def imprimir(self, boton, datos=None):
-        self.creaPDF()
-        os.system('pdf2ps %s - | lpr' % NOMBRE_FICHERO)
-        
     def nuevoAsistente(self, boton, datos=None):
         dialog = self.widgets.get_widget('dlgNuevoAsistente')
         resultado = dialog.run()
@@ -279,53 +166,21 @@ class Acredita:
             # lo meto en la interfaz
             self.listStore.prepend(datos)
 
-
         
     def ticketrow (self, linea=None):
-        
-        
-        
-        
         self.cursor.execute('select unidades, descripcion, precio from articulosa where id = 1')
-        
         for linea in self.cursor.fetchall():
-#            print linea
-                #id = dato[0]
-                #dato = [unicode(d, 'latin-1') for d in dato[1:]]
-                
-                
-            
-            
-            
             ud, nombre, precio = linea
-            #print "Id: %s, Ud: %s, Nombre: %s, Precio: %s" % (id, ud, nombre, precio)
-        #data = []
-        #data.append(ud, nombre, precio)
-        #for a in enumerate(['a','b','c']): print a
-#        for dato in d:
-#            data.append(dato)
-        
-           
-            ID_TICKET = self.insertalinea(linea)
-#            print ID_TICKET
-            datos = [ID_TICKET] + [ud] + [nombre] + [precio]
-#            print datos
-        #print id
-        
-            a = self.ticketstore.append(datos)
-            print a
+            id_ticket = self.insertalinea(linea)
+            linea = [id_ticket] + [ud] + [nombre] + [precio]
+            self.ticketstore.append(linea)
             
+                        
     def insertalinea (self, linea):
-        
         self.cursor.execute('insert into acreditaciones.ventas (cantidad, id_articulo, importe) values (%s, %s, %s)', linea)
-            
         self.cursor.execute('SELECT max(id_ticket) from ventas where ventas.cantidad =%s AND ventas.id_articulo = %s AND ventas.importe=%s', linea)
-        
         return int(self.cursor.fetchone()[0])
                 
-            
-        
-        
     
     def quitaAsistente(self, boton, datos=None):
         seleccion = []
@@ -335,6 +190,15 @@ class Acredita:
             self.borraBD(iter)
             self.listStore.remove(iter)
             
+    def quitalineaticket(self, boton, linea=None):
+        seleccion = []
+        self.treeview3.get_selection().selected_foreach(
+            lambda model, path, iter, sel = seleccion: sel.append(iter))
+        for iter in seleccion:
+            print iter
+            self.borralineaticket(iter)
+            self.ticketstore.remove(iter)
+                
     def run(self):
         gtk.main()
 
@@ -346,7 +210,8 @@ class Acredita:
     def editedcells(self, render, path, newTex, columna):
         iter = self.ticketstore.get_iter(path)
         self.ticketstore.set_value(iter, columna, newTex)
-    
+        self.actualizaticketstore(iter)
+        
     def insertaBD(self, datos):
         tmp = []
         for d in datos:
@@ -362,6 +227,13 @@ class Acredita:
         c.execute('delete from acreditaciones where id = %s',
                   (self.listStore.get_value(iter, ID),))
         
+    
+    def borralineaticket(self, iter):
+        c = self.cursor
+        c.execute('delete from ventas where id_ticket = %s',
+                  (self.ticketstore.get_value(iter, ID_TICKET),))
+        
+                
     def actualizaBD(self, iter):
         c = self.cursor
         c.execute("""update acreditaciones set nombre = %s, direccion = %s,
@@ -372,6 +244,16 @@ class Acredita:
             self.listStore.get_value(iter, IMPORTE).encode('latin-1'),
             self.listStore.get_value(iter, HORA).encode('latin-1'),
             self.listStore.get_value(iter, ID)
+            ))
+        
+    def actualizaticketstore(self, iter):
+        c = self.cursor
+        c.execute("""update ventas set cantidad = %s, id_articulo = %s, importe = %s
+        where id_ticket = %s""", (
+            self.ticketstore.get_value(iter, UNI).encode('latin-1'),
+            self.ticketstore.get_value(iter, DESCRIPCION).encode('latin-1'),
+            self.ticketstore.get_value(iter, IMP).encode('latin-1'),
+            self.ticketstore.get_value(iter, ID_TICKET),
             ))
 
     def salir(self, boton, datos=None):
