aa867e2fc2685a46b98cf6509dc4131bb28c872b
jjrojoc <jjrojoc>
2007-07-06 13:52:59 +0000

    [License] * ADD: COPYING, with license GPLv3.

--- trunk/src/pytpv.py
+++ trunk/src/pytpv.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# PyTPV, software point of sale for restaurant, bar and pizzeria.
+# Copyright (C) 2007 Juan Jose Rojo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Can you contact with author by means of email: <jjrojoc@gmail.com> or the
+# next postal address:
+# Juan Jose Rojo. San Lazaro, 13. 30840 Alhama de Murcia. Murcia. Spain
+# This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; type `show c' for details.
+
+
+
 from encodings import utf_8
 
 from codificacion import *
@@ -435,6 +460,7 @@ class PyTPV:
         for iter in seleccion:
             self.borraBD(iter)
             self.listStore.remove(iter)
+            
             
     def quitalineaticket(self, boton, linea=None):
         seleccion = []
