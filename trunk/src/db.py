607cb893646d338b5126419000742cdb6f30722e
jjrojoc <jjrojoc>
2008-12-25 16:22:09 +0000

    Comment all code in db.py test file.

--- trunk/src/db.py
+++ trunk/src/db.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 #coding=utf-8
-import gtk, gtk.glade
+#import gtk, gtk.glade
 #from gazpacho.loader.loader import ObjectBuilder
 
-from aboutdialog import aboutdialog
-from dialogclients import DialogClients
+#from aboutdialog import aboutdialog
+#from dialogclients import DialogClients
 
 
 
-class pp:
-    def __init__(self):
-        pass
-            
-            
-if __name__=='__main__':
-    a = pp()
-    gtk.main()
+#class pp:
+#    def __init__(self):
+#        pass
+#            
+#            
+#if __name__=='__main__':
+#    a = pp()
+#    gtk.main()
