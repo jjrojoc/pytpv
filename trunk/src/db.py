1739aeb9dc048500605d022cbf270317c65883be
jjrojoc <jjrojoc>
2009-01-06 17:13:59 +0000

    Deleted unused code

--- trunk/src/db.py
+++ trunk/src/db.py
@@ -1,18 +1,2 @@
 #!/usr/bin/env python
 #coding=utf-8
-#import gtk, gtk.glade
-#from gazpacho.loader.loader import ObjectBuilder
-
-#from aboutdialog import aboutdialog
-#from dialogclients import DialogClients
-
-
-
-#class pp:
-#    def __init__(self):
-#        pass
-#            
-#            
-#if __name__=='__main__':
-#    a = pp()
-#    gtk.main()
